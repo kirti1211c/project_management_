bd55c1555b4227f3e93be0e56388c0feb8365cb4
kirti1211c <91460755+kirti1211c@users.noreply.github.com>
2022-12-13 19:40:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -238,11 +238,11 @@ app.get('/userr', (req, res) => {
 })
 
 const port = process.env.PORT || 8080
-connectDB().then(() => {
+// connectDB().then(() => {
     app.listen(port, () => {
         console.log("Listening!");
     })
-}
+// }
 
 
 // Proj.find({}).then(data => {
